30e90ec20ace1e45518bae75b60b1b2559c848a0
Viktor <1180505n@gmail.com>
2024-11-24 20:19:42 +0100

    lesson_06 added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import './App.css';
 
+//lessons
+import Lesson06 from './lessons/Lesson06/Lesson06';
+
+//homeworks
+
+//consultations
+
 function App() {
-  return (
-    <div className="App">
-      My React-TS project
-    </div>
-  );
+    return (
+        <div className="App">
+            <Lesson06 />
+        </div>
+    );
 }
 
 export default App;
