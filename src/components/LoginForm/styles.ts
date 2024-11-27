b439c8f7885fe5a9ff7342be06113a6fb6adfe82
Viktor <1180505n@gmail.com>
2024-11-27 23:41:29 +0100

    Code updated to Consultation04

--- src/components/LoginForm/styles.ts
+++ src/components/LoginForm/styles.ts
@@ -2,24 +2,28 @@
 import styled from "@emotion/styled";
 
 export const LoginFormWrapper = styled.form`
-                                display: flex;
-flex-direction: column;
-gap: 30px;
-width: 590px;
-padding: 60px;
-border: 1px solid black;
-border-radius: 4px;
-font-family: Lato, Geneva, Tahoma, sans-serif;
+    display: flex;
+    flex-direction: column;
+    gap: 30px;
+    width: 590px;
+    padding: 60px;
+    border: 1px solid black;
+    border-radius: 4px;
+    font-family: Lato, Geneva, Tahoma, sans-serif;
+
+    &:hover {
+        background-color: #f7e8be;
+    }
 `;
 
 export const LoginFormTitle = styled.div`
-                              font-size: 24px;
-font-weight: bold;
+    font-size: 24px;
+    font-weight: bold;
 `;
 
 export const InputsContainer = styled.div`
-                               display: flex;
-flex-direction: column;
-gap: 20px;
-width: 100%;
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    width: 100%;
 `;
