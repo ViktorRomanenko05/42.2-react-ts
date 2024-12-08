42842bc26758e9315b588cf90f05920e1f16d141
Viktor <1180505n@gmail.com>
2024-12-08 01:46:12 +0100

    Lesson 10 project completed

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,13 +1,14 @@
 import "./styles.ts";
 import { ButtonProps } from "./types";
 import { MainButton } from './styles';
+import {Spinner} from "../../lessons/Lesson10/styles";
 
-function Button({ name, type = "button", onClick, disabled = false }: ButtonProps) {
+function Button({ name, type = "button", onClick, disabled = false, loading=false}: ButtonProps) {
     // console.log(props.buttonName);
 
     return (
-        <MainButton onClick={onClick} type={type} disabled={disabled}>
-            {name}
+        <MainButton onClick={onClick} type={type} disabled={disabled} >
+            {loading ? <Spinner /> : name}
         </MainButton>
     );
 }
---
+++ src/lessons/Lesson10/Lesson10_practice.tsx
@@ -0,0 +1,92 @@
+import {v4} from "uuid";
+import axios from "axios";
+import {useState, useEffect} from "react";
+import Button from "../../components/Button/Button";
+import {
+    Lesson10container,
+    ButtonsBlock,
+    ResultContainer,
+    DeleteButtonWrapper,
+    FactItem
+} from "./styles";
+
+const CAT_FACT_URL = "https://catfact.ninja/fact";
+
+//интерфейс для типизации получаемого объекта
+interface CatFact {
+    fact: string,
+    length: number
+}
+
+//интерфейс для типизации объекта состоящего из необходимых
+//текста и присвоенного id (используется для удаления)
+interface FactUnit {
+    id: string,
+    factText: string
+}
+
+function Lesson10_practice() {
+
+    const [factsList, setFactsList] = useState<FactUnit[]>([]);
+    const [loadingStatus, setLoadingStatus] = useState<boolean>(false);
+    const [error, setError] = useState<any>(undefined);
+
+    //Запрашиваем данные при монтировании (первом рендеринге) элемента
+    useEffect(() => {
+        if (factsList.length===0){    //Иногда хук срабатывал при переключениях
+        getFact();}                   //между окнами и вкладками, поэтому добавил if
+    }, []);
+
+    //Функция для запроса факта
+    const getFact = async () => {
+        setError(undefined);
+        try {
+            setLoadingStatus(true);
+            const response = await axios.get(CAT_FACT_URL);
+            const newFact: CatFact = response.data;
+            const newFactUnit: FactUnit = {
+                id: v4(),
+                factText: newFact.fact,
+            }
+            setFactsList((previousFacts) => [...previousFacts, newFactUnit]);
+        } catch (error) {
+            setError(error);
+        } finally {
+            setLoadingStatus(false);
+        }
+    }
+
+    //Функция для удаления всех полученных фактов
+    const deleteAllData = (): void => {
+        setFactsList([]);
+    }
+
+    //Функция для удаления отдельного факта
+    const deleteFact = (idForRemove: string) => {
+        setFactsList((previousFacts) => previousFacts.filter((unit) => unit.id !== idForRemove));
+    }
+
+    return (
+        <Lesson10container>
+            <ButtonsBlock>
+                <Button name={"GET MORE INFO"} onClick={getFact} loading={loadingStatus} disabled={loadingStatus} />
+                {factsList.length!==0 &&
+                <Button name={"DELETE ALL DATA"} onClick={deleteAllData}/>}
+            </ButtonsBlock>
+            {factsList.length > 0 && (
+                <ResultContainer>
+                    {factsList.map((factItem) => (
+                        <FactItem key={factItem.id}>
+                            {factItem.factText}
+                            <DeleteButtonWrapper>
+                                <Button name="DELETE" onClick={() => deleteFact(factItem.id)}/>
+                            </DeleteButtonWrapper>
+                        </FactItem>
+                    ))}
+                </ResultContainer>
+            )}
+        </Lesson10container>
+    )
+}
+
+export default Lesson10_practice;
